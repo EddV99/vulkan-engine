c61914797cd46f4f9ce8d37566e979fad2126fc1
Eduardo Valdivia <eddval0808@gmail.com>
2024-05-07 14:37:18 -0600

    create framebuffer(s)

--- src/main.cpp
+++ src/main.cpp
@@ -498,6 +498,29 @@ private:
     vkDestroyShaderModule(device, vertexShaderModule, nullptr);
     vkDestroyShaderModule(device, fragmentShaderModule, nullptr);
 
+    // create framebuffer(s)
+    // ---------------------------------------------------------------------------------------------------------------
+    int i = 0;
+    framebuffers.resize(swapchainImageViews.size());
+    for (auto &imageView : swapchainImageViews) {
+      VkImageView attachments[] = {imageView};
+
+      VkFramebufferCreateInfo framebufferInfo{};
+      framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+      framebufferInfo.renderPass = renderPass;
+      framebufferInfo.attachmentCount = 1;
+      framebufferInfo.pAttachments = attachments;
+      framebufferInfo.width = extent.width;
+      framebufferInfo.height = extent.height;
+      framebufferInfo.layers = 1;
+
+      if (vkCreateFramebuffer(device, &framebufferInfo, nullptr,
+                              &framebuffers[i]) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create framebuffer");
+      }
+      i++;
+    }
+
     // get graphics queue handle
     // ---------------------------------------------------------------------------------------------------------------
     vkGetDeviceQueue(device, queueFamily.graphics.value(), 0, &graphicsQueue);
@@ -554,10 +577,12 @@ private:
   }
 
   void clean() {
+    for (auto framebuffer : framebuffers) {
+      vkDestroyFramebuffer(device, framebuffer, nullptr);
+    }
     vkDestroyPipeline(device, graphicsPipeline, nullptr);
     vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
     vkDestroyRenderPass(device, renderPass, nullptr);
-
     for (auto imageView : swapchainImageViews) {
       vkDestroyImageView(device, imageView, nullptr);
     }
@@ -685,6 +710,7 @@ private:
   VkRenderPass renderPass;
   VkPipelineLayout pipelineLayout;
   VkPipeline graphicsPipeline;
+  std::vector<VkFramebuffer> framebuffers;
 
   const std::vector<const char *> validationLayers = {
       "VK_LAYER_KHRONOS_validation"};
