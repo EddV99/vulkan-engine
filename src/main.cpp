c57f6405fc2005c9fd1f759ded67357547d99290
Eduardo Valdivia <eddval0808@gmail.com>
2024-05-07 10:53:32 -0600

    setup the render pass object

--- src/main.cpp
+++ src/main.cpp
@@ -299,6 +299,39 @@ private:
       }
     }
 
+    // setup render pass
+    // ---------------------------------------------------------------------------------------------------------------
+    VkAttachmentDescription colorAttachment{};
+    colorAttachment.format = swapchainImageFormat;
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference colorAttachmentRef{};
+    colorAttachmentRef.attachment = 0;
+    colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass{};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorAttachmentRef;
+
+    VkRenderPassCreateInfo renderPassInfo{};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderPassInfo.attachmentCount = 1;
+    renderPassInfo.pAttachments = &colorAttachment;
+    renderPassInfo.subpassCount = 1;
+    renderPassInfo.pSubpasses = &subpass;
+
+    if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) !=
+        VK_SUCCESS) {
+      throw std::runtime_error("Failed to create render pass");
+    }
+
     // setup graphics pipeline
     // ---------------------------------------------------------------------------------------------------------------
     auto vertexShader = readFile("../src/shaders/vert.spv");
@@ -499,6 +532,7 @@ private:
 
   void clean() {
     vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+    vkDestroyRenderPass(device, renderPass, nullptr);
 
     for (auto imageView : swapchainImageViews) {
       vkDestroyImageView(device, imageView, nullptr);
@@ -624,6 +658,7 @@ private:
   std::vector<VkImageView> swapchainImageViews;
   VkFormat swapchainImageFormat;
   VkExtent2D swapchainExtent;
+  VkRenderPass renderPass;
   VkPipelineLayout pipelineLayout;
 
   const std::vector<const char *> validationLayers = {
