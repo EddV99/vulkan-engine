7fd6b152af417d3900525bfceaf9718a68c8ec48
Eduardo Valdivia <eddval0808@gmail.com>
2024-05-14 21:03:34 -0600

    Work on renderer

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -2,11 +2,87 @@
  * @file renderer.cpp
  */
 
-#include "renderer.hpp"
-#include "../util/util.hpp"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 #include <vulkan/vulkan_core.h>
 
+#include "../util/util.hpp"
+#include "renderer.hpp"
+
+#include <cstring>
+
 namespace Renderer {
+
+void Renderer::init() {
+  if (enableValidationLayers && !checkValidationLayerSupport()) {
+    Util::Error("Validation layers requested, but failed to get");
+  }
+  createWindow();
+  createInstance();
+}
+void Renderer::createWindow() {
+  glfwInit();
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  window = glfwCreateWindow(WIDTH, HEIGHT, "Beast Gohan", nullptr, nullptr);
+}
+
+void Renderer::createInstance() {
+  VkApplicationInfo appInfo{};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Beast Gohan";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "Double-D";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+
+  uint32_t glfwExtensionCount = 0;
+  const char **glfwExtensions;
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+  createInfo.enabledExtensionCount = glfwExtensionCount;
+  createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+  if (enableValidationLayers) {
+    createInfo.enabledLayerCount = (uint32_t)validationLayers.size();
+    createInfo.ppEnabledLayerNames = validationLayers.data();
+  } else {
+    createInfo.enabledLayerCount = 0;
+  }
+
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+    Util::Error("Failed to create instance");
+  }
+}
+
+bool Renderer::checkValidationLayerSupport() {
+  uint32_t layerCount;
+  vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+  std::vector<VkLayerProperties> availableLayers(layerCount);
+  vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+  bool layerFound;
+  for (const auto &layer : validationLayers) {
+    layerFound = false;
+    for (const auto &layerProperties : availableLayers) {
+      if (strcmp(layer, layerProperties.layerName) == 0) {
+        layerFound = true;
+        break;
+      }
+    }
+    if (!layerFound)
+      return false;
+  }
+  return true;
+}
 
 void Renderer::createShaders(std::string vertexFilePath,
                              std::string fragmentFilePath,
--- src/renderer/renderer.hpp
+++ src/renderer/renderer.hpp
@@ -8,6 +8,9 @@
 
 #pragma once
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <string>
 #include <vector>
 #include <vulkan/vulkan_core.h>
@@ -21,13 +24,25 @@ const bool enableValidationLayers = true;
 
 class Renderer {
 public:
+  void draw();
+  void init();
+
 private:
+  const std::vector<const char *> validationLayers = {
+      "VK_LAYER_KHRONOS_validation"};
+
+  bool checkValidationLayerSupport();
+
+  void createWindow();
+  void createInstance();
+
   struct Shaders {
     VkShaderModule vertex = nullptr;
     VkShaderModule fragment = nullptr;
     VkShaderModule tesselation = nullptr;
     VkShaderModule geometery = nullptr;
   } shaders;
+
   void createShaders(std::string vertexFilePath, std::string fragmentFilePath,
                      std::string tesselationFilePath = nullptr,
                      std::string geometeryFilePath = nullptr);
@@ -42,9 +57,12 @@ private:
 
   void createUniform();
 
-  void draw();
-
   // Instance Variables
   VkDevice device;
+  VkInstance instance;
+  GLFWwindow *window;
+
+  const uint32_t WIDTH = 500; 
+  const uint32_t HEIGHT = 500; 
 };
 } // namespace Renderer
