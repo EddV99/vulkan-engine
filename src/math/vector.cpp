d91435fee1051c8665d857bffbc0014e29a1eeb3
Eduardo Valdivia <eddval0808@gmail.com>
2024-05-20 18:44:47 -0600

    update vector class

--- src/math/vector.cpp
+++ src/math/vector.cpp
@@ -7,59 +7,52 @@ namespace Math {
 // =============================================================================
 
 Vector3::Vector3(float x, float y, float z) {
-  data[0] = x;
-  data[1] = y;
-  data[2] = z;
+  this->x = x;
+  this->y = y;
+  this->z = z;
 }
 
-float Vector3::x() { return data[0]; }
-float Vector3::y() { return data[1]; }
-float Vector3::z() { return data[2]; }
-
 Vector3 Vector3::operator+(Vector3 &right) {
-  return Vector3(this->data[0] + right.data[0], this->data[1] + right.data[1],
-                 this->data[2] + right.data[2]);
+  return Vector3(x + right.x, this->y + right.y, this->z + right.z);
 }
 
 Vector3 Vector3::operator-(Vector3 &right) {
-  return Vector3(this->data[0] - right.data[0], this->data[1] - right.data[1],
-                 this->data[2] - right.data[2]);
+  return Vector3(x - right.x, this->y - right.y, this->z - right.z);
 }
 
 Vector3 &Vector3::operator=(Vector3 other) noexcept {
-  this->data[0] = other.data[0];
-  this->data[1] = other.data[1];
-  this->data[2] = other.data[2];
+  x = other.x;
+  this->y = other.y;
+  this->z = other.z;
   return *this;
 }
 
 float Vector3::dot(Vector3 &right) {
-  return this->x() * right.x() + this->y() * right.y() + this->z() * right.z();
+  return this->x * right.x + this->y * right.y + this->z * right.z;
 }
 
 Vector3 Vector3::cross(Vector3 &right) {
-  return Vector3(this->y() * right.z() - this->z() * right.y(),
-                 -(this->x() * right.z() - this->z() * right.x()),
-                 this->x() * right.y() - this->y() * right.x());
+  return Vector3(this->y * right.z - this->z * right.y,
+                 -(this->x * right.z - this->z * right.x),
+                 this->x * right.y - this->y * right.x);
 }
 
 float Vector3::length() {
-  return std::sqrt(this->x() * this->x() + this->y() * this->y() +
-                   this->z() * this->z());
+  return std::sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
 }
 
 void Vector3::normalize() {
   float l = 1.0f / this->length();
-  data[0] = data[0] * l;
-  data[1] = data[1] * l;
-  data[2] = data[2] * l;
+  x = x * l;
+  y = y * l;
+  z = z * l;
 }
 Vector3 Vector3::normal() {
   float l = 1.0f / this->length();
   Vector3 result;
-  result.data[0] = data[0] * l;
-  result.data[1] = data[1] * l;
-  result.data[2] = data[2] * l;
+  result.x = x * l;
+  result.y = y * l;
+  result.z = z * l;
   return result;
 }
 
@@ -68,15 +61,10 @@ Vector3 Vector3::normal() {
 // =============================================================================
 
 Vector4::Vector4(float x, float y, float z, float w) {
-  data[0] = x;
-  data[1] = y;
-  data[2] = z;
-  data[3] = w;
+  this->x = x;
+  this->y = y;
+  this->z = z;
+  this->w = w;
 }
-
-float Vector4::x() { return data[0]; }
-float Vector4::y() { return data[1]; }
-float Vector4::z() { return data[2]; }
-float Vector4::w() { return data[3]; }
 
 }; // namespace Math
--- src/math/vector.hpp
+++ src/math/vector.hpp
@@ -4,50 +4,44 @@
 
 #pragma once
 
+#include "../util/defines.hpp"
+
 namespace Math {
+
 class Vector3 {
 public:
-  Vector3(float x = 0, float y = 0, float z = 0);
-
-  float x();
-  float y();
-  float z();
+  Vector3(f32 x = 0, f32 y = 0, f32 z = 0);
 
   Vector3 operator+(Vector3 &right);
   Vector3 operator-(Vector3 &right);
   Vector3 &operator=(Vector3 other) noexcept;
 
-  float dot(Vector3 &right);
+  f32 dot(Vector3 &right);
   Vector3 cross(Vector3 &right);
-  float length();
+  f32 length();
   void normalize();
   Vector3 normal();
 
 private:
-  float data[3];
+  f32 x, y, z;
 };
 
 class Vector4 {
 public:
-  Vector4(float x = 0, float y = 0, float z = 0, float w = 0);
-
-  float x();
-  float y();
-  float z();
-  float w();
+  Vector4(f32 x = 0, f32 y = 0, f32 z = 0, f32 w = 0);
 
   Vector4 operator+(Vector4 &right);
   Vector4 operator-(Vector4 &right);
   Vector4 &operator=(Vector4 other) noexcept;
 
-  float dot(Vector4 &right);
+  f32 dot(Vector4 &right);
   Vector4 cross(Vector4 &right);
-  float length();
+  f32 length();
   void normalize();
   Vector4 normal();
 
 private:
-  float data[4];
+  f32 x, y, z, w;
 };
 
 }; // namespace Math
