d442c3a978573c421e099f696203c841aa747b6d
Eduardo Valdivia <eddval0808@gmail.com>
2024-06-05 22:44:34 -0600

    some obj files for testing

--- src/entry.cpp
+++ src/entry.cpp
@@ -9,9 +9,13 @@
 #include <iostream>
 
 int main(void) {
+  //std::string meshFilePath = "res/cube/cube.obj";
+  //std::string meshFilePath = "res/teapot/teapot.obj";
+  std::string meshFilePath = "res/sphere/sphere.obj";
+
   try {
 
-    Game::Object obj("res/cube/cube.obj");
+    Game::Object obj(meshFilePath);
     std::vector<Game::Object> objs;
     objs.push_back(obj);
     Game::Scene scene(objs);
